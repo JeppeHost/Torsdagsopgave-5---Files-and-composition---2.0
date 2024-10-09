3951af609c24a5136bf89516e18e05b6650b55ba
JeppeHost <157792815+JeppeHost@users.noreply.github.com>
2024-10-09 21:16:37 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,65 +1,15 @@
-package Task3;
+package Task2;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-
-        ArrayList<Room> rooms = new ArrayList<>();
-
-        Room room1 = new Room(3, 5);
-        Room room2 = new Room(5, 6);
-        Room room3 = new Room(6, 6);
-
-        rooms.add(room1);
-        rooms.add(room2);
-        rooms.add(room3);
-
-        Building building1 = new Building(20, rooms);
-        Building building2 = new Building(2, rooms);
-
-        System.out.println("Number of lamps in building1: " + countLampsInBuilding(building1));
-        System.out.println("Number of windows in building1: " + countWindowsInBuilding(building1));
-        System.out.println("Number of rooms in building1: " + countRoomsInBuilding(building1));
-
-
-        System.out.println("Building1 is normal: " + isNormal(building1));
-        System.out.println("Building2 is normal: " + isNormal(building2));
-
-    }
-
-    public static int countLampsInBuilding(Building b){
-        int totalLamps = 0;
-        for (Room room : b.getRooms()) {
-           totalLamps += room.getNumberOfLamps();
+    public static void main(String [] args){
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
+        ArrayList<String> coffeeMenu = cafe.getCoffeeMenu();
+        for(int i = 0; i < coffeeMenu.size(); i++){
+            System.out.println(coffeeMenu.get(i));
         }
-        return totalLamps;
-    }
 
-    public static int countWindowsInBuilding(Building b){
-        int totalWindows = 0;
-        for (Room room : b.getRooms()) {
-            totalWindows += room.getNumberOfWindows();
-        }
-        return totalWindows;
-    }
 
-    public static int countRoomsInBuilding(Building b){
-        int totalRooms = 0;
-        for (Room room : b.getRooms()) {
-            totalRooms++;
-        }
-        return totalRooms;
-    }
-
-    public static boolean isNormal(Building b){
-
-        if(b.getNumberOfFloors() >= b.getRooms().size()){
-            return true;
-        } else {
-            return false;
-        }
     }
 }
-
-
-
